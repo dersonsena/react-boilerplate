8da72e739b8ad1952e7403ed6c5a5c8751199a86
Kilderson Sena <dersonsena@gmail.com>
2021-03-16 20:44:06 -0300

    chore: implementation of path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import ComponentA from '@/components/component-a/ComponentA'
 import React from 'react'
 
 const App: React.FunctionComponent = () => (
-  <div><h1>React App</h1></div>
+  <div>
+    <h1>React App</h1>
+    <ComponentA />
+  </div>
 )
 
 export default App
---
+++ src/components/component-a/ComponentA.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ComponentA: React.FC = () => (
+    <h1>Componente A</h1>
+)
+
+export default ComponentA
