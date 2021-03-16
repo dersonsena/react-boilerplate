101cfdb390190595970b9050b736231477435e5b
Kilderson Sena <dersonsena@gmail.com>
2021-03-16 18:54:38 -0300

    feat: first files and settings of boilerplate

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/tests/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
